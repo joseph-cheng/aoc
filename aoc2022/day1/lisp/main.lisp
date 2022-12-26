2cd5b89bbe9a881a1bbcf293caf7a8cee524da26
Joe Cheng <joe.cheng2001@gmail.com>
2022-12-26 12:57:56 +0000

    day 1 lisp + emacs gitignore

---
+++ aoc2022/day1/lisp/main.lisp
@@ -0,0 +1,26 @@
+(defun parse (filename)
+  (loop
+    for x in (uiop:read-file-lines filename)
+    with curr-list = (list)
+    with result = (list)
+    when (not (zerop (length x)))
+      do (push (parse-integer x) curr-list)
+    else
+      do (push curr-list result) (setf curr-list (list))
+    finally
+       (return (push curr-list result)))
+  )
+
+(defun sums (l)
+  (loop
+    for x in l
+    collect (loop for y in x sum y))
+  )
+
+(defun day1 (f)
+  (car (sort (sums (parse f)) '>)))
+
+(defun day2 (f)
+  (let ((x (sort (sums (parse f)) '>)))
+    (+ (nth 0 x) (nth 1 x) (nth 2 x))
+    ))
