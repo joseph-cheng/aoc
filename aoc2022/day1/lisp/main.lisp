6af8a817b9bc5333662b7b8927da4fba8006ec98
Joe Cheng <joe.cheng2001@gmail.com>
2022-12-26 18:14:33 +0000

    day 1 lisp renaming + day 2 lisp

--- aoc2022/day1/lisp/main.lisp
+++ aoc2022/day1/lisp/main.lisp
@@ -17,10 +17,10 @@
     collect (loop for y in x sum y))
   )
 
-(defun day1 (f)
+(defun part1 (f)
   (car (sort (sums (parse f)) '>)))
 
-(defun day2 (f)
+(defun part2 (f)
   (let ((x (sort (sums (parse f)) '>)))
     (+ (nth 0 x) (nth 1 x) (nth 2 x))
     ))
